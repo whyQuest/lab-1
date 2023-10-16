cbb799e5d82c2d22a92ea7cbf8afffef9bb84488
whyQuest <igor.ampilogov004@gmail.com>
2023-10-16 10:52:29 +0300

    Добавьте файлы проекта.

---
+++ лаба 1/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+
+namespace лаба_1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            house a;
+              int entrances1;
+        int floors1;
+            int apartmentsPerFloor1;
+            double priceApart1;
+
+            if (int.TryParse(podezd.Text, out entrances1) && int.TryParse(poverx.Text, out floors1) && int.TryParse(kva.Text, out apartmentsPerFloor1) && double.TryParse(cena.Text, out priceApart1))
+            {
+                a = new house(entrances1, entrances1, entrances1, priceApart1);
+                int ApartmentsPerFloor1 = a.CountApInEnt();
+                int CountApInHouse1 = a.CountApInHouse();
+                double CostApInHouse1 = a.CostApInHouse();
+                r1.Text = $"К-ть квартир у під'їзді {ApartmentsPerFloor1}";
+                r2.Text = $"К-ть квартир у будинку {CountApInHouse1}";
+                r3.Text = $"Ціна квартир у будинку {CostApInHouse1}";
+            }
+            else
+            {
+                r1.Text = "Помилка: Невірний формат введених даних.";
+                r2.Text = "Помилка: Невірний формат введених даних.";
+                r3.Text = "Помилка: Невірний формат введених даних.";
+            }
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ лаба 1/house.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace лаба_1
+{
+    internal class house
+    {
+        private int entrances;
+        private int floors;
+        private int apartmentsPerFloor;
+        private double priceApart;
+
+        public house(int entrances, int floors, int apartmentsPerFloor, double priceApart)
+        {
+            if (entrances <= 0)
+                throw new ArgumentException("Кількість під'їздів має бути більше нуля.");
+
+            if (floors <= 0)
+                throw new ArgumentException("Кількість поверхів має бути більше нуля.");
+
+            if (apartmentsPerFloor <= 0)
+                throw new ArgumentException("Кількість квартир на поверсі має бути більше нуля.");
+
+            if (priceApart <= 0)
+                throw new ArgumentException("Вартість квартири має бути більше нуля.");
+
+            this.entrances = entrances;
+            this.floors = floors;
+            this.apartmentsPerFloor = apartmentsPerFloor;
+            this.priceApart = priceApart;
+        }
+
+        public int Entrances
+        {
+            get { return entrances; }
+        }
+
+        public int Floors
+        {
+            get { return floors; }
+        }
+
+        public int ApartmentsPerFloor
+        {
+            get { return apartmentsPerFloor; }
+        }
+
+        public double PriceApart
+        {
+            get { return priceApart; }
+        }
+
+        public int CountApInEnt()
+        {
+            int countApInEnt = apartmentsPerFloor * floors;
+            return countApInEnt;
+        }
+
+        public int CountApInHouse()
+        {
+            int CountApInHouse = CountApInEnt() * entrances;
+            return CountApInHouse;
+        }
+
+
+        public double CostApInHouse()
+        {
+            double Cost = CountApInHouse() * priceApart;
+            return Cost;
+        }
+    }
+}
